a0ed8986881abc0b0a8dd1b714e101d7920dca81
Nishanth-R10 <nishanth.r@omnicoredigital.com>
2025-09-17 11:59:29 +0530

    Delete lib/core/providers directory

--- lib/core/providers/app_providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:alb/feautres/profile/application/profile_notifier.dart';
-import 'package:alb/feautres/profile/data/repository/profile_repository.dart';
-
-import 'package:flutter_riverpod/legacy.dart';
-
-
-final profileNotifierProvider = StateNotifierProvider<ProfileNotifier, ProfileState>(
-  (ref) => ProfileNotifier(ProfileRepository()),
-);
