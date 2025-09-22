7b15b51595cabbe96a0c73d7db13729918b32613
Nishanth-R10 <nishanth.r@omnicoredigital.com>
2025-09-22 14:50:49 +0530

    Delete lib/feautres/profile directory

--- lib/feautres/profile/data/repository/profile_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../../domain/entities/profile.dart';
-
-class ProfileRepository {
-  Profile getProfile() {
-    return Profile(name: "Alqabiadi", location: "alabama", id: "00052321");
-  }
-}
